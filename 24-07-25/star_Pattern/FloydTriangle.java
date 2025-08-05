787c6e15e48b41750c95d9eff1f0b5cc2ec7ef86
Kumar Yash <143955711+kumaryash-24@users.noreply.github.com>
2025-08-05 10:33:50 +0530

    Update FloydTriangle.java

--- 24-07-25/star_Pattern/FloydTriangle.java
+++ 24-07-25/star_Pattern/FloydTriangle.java
@@ -5,10 +5,11 @@ public class FloydTriangle {
 
         for (int i = 1; i <= 5; i++) {  
             for (int j = 1; j <= i; j++) {
-                System.out.print(number + " ");
+                System.out.print(number + " ");                   
                 number++; 
             }
             System.out.println();
         }
     }
 }
+
