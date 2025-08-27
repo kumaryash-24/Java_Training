342cac3f42c42152cb7afce11e9747cfa291c954
Kumar Yash <143955711+kumaryash-24@users.noreply.github.com>
2025-08-27 14:24:55 +0530

    Update LeftTriangle.java

--- 24-07-25/star_Pattern/LeftTriangle.java
+++ 24-07-25/star_Pattern/LeftTriangle.java
@@ -7,8 +7,9 @@ public class LeftTriangle {
         for (int i = 1; i <= 5; i++) {
             for (int j = 1; j <= i; j++) {
                 System.out.print("*");
-            }
+            }               
             System.out.println();
         }
     }
 }
+
