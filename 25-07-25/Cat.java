8ac97ec983394ae44bc6445a5ac7368322ced3c2
Kumar Yash <143955711+kumaryash-24@users.noreply.github.com>
2025-08-29 11:10:59 +0530

    Update Cat.java

--- 25-07-25/Cat.java
+++ 25-07-25/Cat.java
@@ -9,9 +9,10 @@ public class Cat extends Animal {
                                
     public void sound() {
         System.out.println("Cat meows");                                                                             
-    }             
+    }                           
 }
               
+
 
 
 
