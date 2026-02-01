e41bb54df4ed26398c5f1b8bcd29c5e819d4bb14
Kumar Yash <143955711+kumaryash-24@users.noreply.github.com>
2026-02-01 20:44:04 +0530

    Remove excessive whitespace in Cat.java

--- 25-07-25/Cat.java
+++ 25-07-25/Cat.java
@@ -5,13 +5,14 @@ public class Cat extends Animal {
     public Cat() {   
         super("Cat"); 
     }                    
-                                                           
+                                                              
                                           
     public void sound() {  
         System.out.println("Cat meows");                                                                                        
     }                           
 }
               
+
 
 
 
