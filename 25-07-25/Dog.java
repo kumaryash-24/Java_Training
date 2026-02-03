9b2514acd7abfb9546acc66c2e3b82d279a98afc
Kumar Yash <143955711+kumaryash-24@users.noreply.github.com>
2026-02-03 16:15:01 +0530

    Refactor Dog class formatting

--- 25-07-25/Dog.java
+++ 25-07-25/Dog.java
@@ -4,7 +4,7 @@ public class Dog extends Animal {
 
     
     public Dog() {
-        super("Dog");    
+        super("Dog");      
     }   
 
     
@@ -15,6 +15,7 @@ public class Dog extends Animal {
 }
 
  
+
 
 
 
